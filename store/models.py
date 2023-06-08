f2a3d75d57fa7128fa3f536b9ef28881e8f7a326
shazad <shazadalibaig@gmail.com>
2023-06-08 14:01:01 +0530

    store and cart

--- store/models.py
+++ store/models.py
@@ -22,4 +22,28 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+
+class VariationManager(models.Manager):
+    def colours(self):
+        return super(VariationManager,self).filter(variation_category='colour',is_active=True)
+    
+    def sizes(self):
+        return super(VariationManager,self).filter(variation_category='size',is_active=True)
+
+variation_category_choice =(
+    ('colour','colour'),
+    ('size','size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100,choices=variation_category_choice)
+    variation_values = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_values
     
